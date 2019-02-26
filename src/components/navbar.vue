74fb3b17da90d96595e07b6a4d06ad48c9b40121
mru24 <mru24@yahoo.com>
2019-02-26 00:33:20 +0000

    added toolbar

---
+++ src/components/navbar.vue
@@ -0,0 +1,47 @@
+<template>
+  <nav>
+    <v-toolbar app flat>
+      <v-toolbar-side-icon class="grey--text" @click="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title class="grey--text">
+        <span class="font-weight-light">ADMIN</span>
+        <span class="underline">DASHBOARD</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn flat left class="grey--text">
+        <span>Login</span>
+        <v-icon right>lock_open</v-icon>
+      </v-btn>
+
+    </v-toolbar>
+    <v-navigation-drawer class="indigo pa-4" v-model="drawer" app>
+      <v-list>
+        <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
+          <v-list-tile-action>
+            <v-icon class="white--text">
+              {{ link.icon }}
+            </v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title class="white--text">
+              {{ link.text }}
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      drawer: false,
+      links: [
+        {icon: 'dashboard', text: 'Dashboard', route: '/'},
+        {icon: 'info', text: 'About', route: '/about'}
+      ]
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,44 @@
 <template>
-  <HelloWorld />
+  <v-container>
+    <h1>Home</h1>
+    <v-layout class="my-3" row wrap>
+      <v-flex class="pa-2" md9 xs12>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </v-flex>
+      <v-flex class="pa-2" md3 xs12>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      </v-flex>
+    </v-layout>
+
+    <v-btn class="green darken-2" dark>
+      <v-icon left>web</v-icon>
+      <span>wwproject.eu</span>
+    </v-btn>
+
+    <v-btn class="yellow darken-2">
+      <v-icon left>email</v-icon>
+      <span>email me</span>
+    </v-btn>
+
+    <v-layout class="my-3" row wrap>
+      <v-flex class="pa-2" md6 xs12>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </v-flex>
+      <v-flex class="pa-2" md6 xs12>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
 
-  export default {
-    components: {
-      HelloWorld
-    }
-  }
+export default {
+
+}
+
 </script>
